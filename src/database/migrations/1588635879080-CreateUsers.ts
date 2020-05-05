3507405d68bfbd2e269cd7e7ee96a3e24c6a7a69
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-05-04 22:23:43 -0300

    feature: User

--- src/database/migrations/1588635879080-CreateUsers.ts
+++ src/database/migrations/1588635879080-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1588544630757
-  implements MigrationInterface {
+export default class CreateUsers1588635879080 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,19 @@ export default class CreateAppointments1588544630757
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider_id',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
             isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -39,7 +43,7 @@ export default class CreateAppointments1588544630757
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('users');
   }
 }
