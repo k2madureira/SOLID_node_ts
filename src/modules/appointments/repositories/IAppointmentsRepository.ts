90cb0d0f32e229cf5d24a9cfe2e2e9ddea20bf49
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-08 20:44:39 -0300

    controllers

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -4,4 +4,5 @@ import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  list(): Promise<Appointment | undefined>;
 }
