f68e6cc2399b8602ef03a025d212faff15b218b2
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-10 20:39:08 -0300

    test with FakeRepository

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -4,5 +4,5 @@ import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
-  list(): Promise<Appointment | undefined>;
+  find(): Promise<Appointment[] | undefined>;
 }
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('soma dois numeros', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1225479',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  /* it('should not be able to create two appointments on the same time', () => {
+    expect(1 + 2).toBe(3);
+  }); */
 });
