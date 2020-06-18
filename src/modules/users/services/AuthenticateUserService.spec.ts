731cf76d8ae0d41af3200636b27ba1a13d812197
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-18 19:50:23 -0300

    coverage code erros

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
@@ -32,5 +32,49 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUser(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'john@exemple.com',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUser(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'john@exemple.com',
+      password: '123',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'john@exemple.com',
+        password: '456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
