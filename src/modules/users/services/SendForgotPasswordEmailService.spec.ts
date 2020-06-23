d3bc9dc6b2b2bdbbf9ff1807d13697d95ba0b138
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-22 21:18:16 -0300

    TDD: SendMail GREEN

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -14,6 +14,7 @@ describe('SendForgotPasswordEmail', () => {
 
     const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
       fakeUsersRepository,
+      fakeMailProvider,
     );
 
     await fakeUsersRepository.create({
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 // import AppError from '@shared/errors/AppError';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 
 // import User from '../infra/typeorm/entities/User';
@@ -13,7 +14,12 @@ export default class SendForgotPasswordEmailService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
   ) {}
 
-  public async execute(data: IRequest): Promise<void> {}
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(email, 'Recover Email');
+  }
 }
