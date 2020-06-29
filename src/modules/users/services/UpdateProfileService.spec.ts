1824a2931864a8d07ae568cb1270bf7af8dbdeba
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-29 19:36:58 -0300

    TDD: UpdateProfileService 2º test

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -1,6 +1,6 @@
 // import AppError from '@shared/errors/AppError';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
@@ -36,5 +36,27 @@ describe('UpdateProfile', () => {
 
     expect(updatedUser.name).toBe('John Dummy');
     expect(updatedUser.email).toBe('john@dummy.com');
+  });
+
+  it('should not be able to change to another user email', async () => {
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'jhon@exemple.com',
+      password: '123',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe wtf',
+      email: 'jhonwtf@exemple.com',
+      password: '123',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'John Dummy',
+        email: 'johndoe@dummy.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
