036326432f8cbe0d7dfe97ae2d1a1bf424f1ef3f
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-29 19:54:21 -0300

    new TDD UpdateProfile

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -55,7 +55,42 @@ describe('UpdateProfile', () => {
       updateProfile.execute({
         user_id: user.id,
         name: 'John Dummy',
-        email: 'johndoe@dummy.com',
+        email: 'jhon@exemple.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'jhon@exemple.com',
+      password: '123',
+    });
+
+    const updatedUser = await updateProfile.execute({
+      user_id: user.id,
+      name: 'John Dummy',
+      email: 'john@dummy.com',
+      old_password: '123',
+      password: '123123',
+    });
+
+    expect(updatedUser.password).toBe('123123');
+  });
+
+  it('should be able to update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'jhon@exemple.com',
+      password: '123',
+    });
+
+    await expect(
+      updateProfile.execute({
+        user_id: user.id,
+        name: 'John Dummy',
+        email: 'john@dummy.com',
+        password: '123123',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -24,15 +24,38 @@ export default class UpdateProfile {
     private hashProvider: IHashprovider,
   ) {}
 
-  public async execute({ user_id, name, email }: IRequest): Promise<User> {
+  public async execute({
+    user_id,
+    name,
+    email,
+    old_password,
+    password,
+  }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User not found');
     }
 
+    const userWithUpdateEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user_id) {
+      throw new AppError('E-mail already in use!');
+    }
+
     user.name = name;
     user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        'Is necessarry the old password for update this field',
+      );
+    }
+
+    if (password) {
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
     return this.usersRepository.save(user);
   }
 }
