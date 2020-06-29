1bca08db217e2f267a97b29963465a05d4cb0984
Lenilson Madureira <lenilsonmadureira@gmail.com>
2020-06-29 19:09:34 -0300

    UpdateProfile service

---
+++ src/modules/users/services/UpdadeProfileService.ts
@@ -0,0 +1,30 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+import IHashprovider from '../providers/HashProvider/models/IHashProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+export default class UpdateProfile {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashprovider,
+  ) {}
+
+  public async execute({ user_id, name, email }: IRequest): Promise<void> {
+    return user;
+  }
+}
